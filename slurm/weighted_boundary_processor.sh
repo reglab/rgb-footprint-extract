bd2c74819e682d81dd87bf991f1500b877bea0b9
Nathan Jo <nathanjo@sls-314-mbpro2021.local>
2022-08-30 23:59:46 -0700

    wt slurm

--- slurm/weighted_boundary_processor.sh
+++ slurm/weighted_boundary_processor.sh
@@ -7,8 +7,9 @@
 #SBATCH --time=04:00:00
 #SBATCH --cpus-per-task=6
 #SBATCH --mem=64GB
+#SBATCH --dependency=afterany:61162143
 
 cd ../
 
 singularity exec $GROUP_HOME/singularity/rgb-building1.sif python3 datasets/converters/weighted_boundary_processor_parallel.py --w=10 --sigma=7.5 --window=150 \
-	--oak-fp=/oak/stanford/groups/deho/building_compliance/san_jose_naip_512/2018/
+	--oak-fp=/oak/stanford/groups/deho/building_compliance/san_jose_naip_512/2016/
