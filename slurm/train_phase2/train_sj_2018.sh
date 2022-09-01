d28c4195bc758711c5e9770988badfad45449de3
Nathan Jo <nathanjo@sls-314-mbpro2021.local>
2022-09-01 11:17:11 -0700

    infer mult bash file

--- slurm/train_phase2/train_sj_2018.sh
+++ slurm/train_phase2/train_sj_2018.sh
@@ -13,5 +13,5 @@ cd ../../
 singularity exec --nv $GROUP_HOME/singularity/rgb-building1.sif python3 run_deeplab.py --backbone=drn_c42 --out-stride=8 --dataset=OSM \
     --workers=4 --loss-type=wce_dice --fbeta=0.2 --epochs=70 --batch-size=16 --test-batch-size=4 --weight-decay=1e-4 \
     --gpu-ids=0,1,2 --lr=5e-4 --loss-weights=1.0,1.0 --dropout=0.3,0.5 --incl-bounds --resume=crowdAI --best-miou --freeze-bn \
-    --data-root=/oak/stanford/groups/deho/building_compliance/san_jose_naip_512/2016/superresx2/ --loss-weights-param=1.03 --preempt-robust \
+    --data-root=/oak/stanford/groups/deho/building_compliance/san_jose_naip_512/2018/superresx2/ --loss-weights-param=1.03 --preempt-robust \
     --superres=2 --use-wandb --year=2018
