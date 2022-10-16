0f052288c77efe00dde06544eaa26e36ff2f75c1
Nathan Jo <nathanjo@sls-314-mbpro2021.local>
2022-10-15 19:11:36 -0700

    fix infer 2020

--- slurm/infer/infer_past_naip_2020.sh
+++ slurm/infer/infer_past_naip_2020.sh
@@ -11,7 +11,7 @@
 cd ../../
 
 singularity exec --nv $GROUP_HOME/singularity/rgb-building1.sif python3 run_deeplab.py --minference --backbone=drn_c42 --out-stride=8 \
-    --workers=2 --epochs=1 --test-batch-size=1 --gpu-ids=0 --resume=SJ_0.2_True_0.0005_0.0001_1.03_24_superresx2
+    --workers=2 --epochs=1 --test-batch-size=1 --gpu-ids=0 --resume=SJ_0.2_True_0.0005_0.0001_1.03_24_superresx2 \
     --window-size=256 --stride=256 --best-miou --dataset=OSM \
     --data-root='/oak/stanford/groups/deho/building_compliance/san_jose_naip_512/2020/superresx2/' \
     --output-dir='/oak/stanford/groups/deho/building_compliance/san_jose_naip_512/2020/infer'
